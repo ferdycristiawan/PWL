14d84355676fa53c42b1209f898b93ba5e405d9e
Ferdy Cristiawan <91183670+ferdycristiawan@users.noreply.github.com>
2023-10-01 23:30:14 +0700

    Add files via upload

---
+++ Pert5/Latihan/env.js
@@ -0,0 +1,13 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "live_wMXD1wTRdb3EGfoT2UbMZsxeSc3pxVYJDjOQs4stCIKKULOrFxKvPBef9lUbT6gT",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_wMXD1wTRdb3EGfoT2UbMZsxeSc3pxVYJDjOQs4stCIKKULOrFxKvPBef9lUbT6gT",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export { dog_env };
